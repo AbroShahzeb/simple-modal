a5a406bdff5777ed3b77e004a8d7f2acd6ee53e2
AbroShahzeb <khizr772@gmail.com>
2024-03-21 15:43:10 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const button = document.querySelectorAll('.show-modal');
+const closeBtn = document.querySelector('.close-modal');
+
+function showModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function hideModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') hideModal();
+});
+
+overlay.addEventListener('click', hideModal);
+
+button.forEach(btn => {
+  btn.addEventListener('click', showModal);
+});
+
+closeBtn.addEventListener('click', hideModal);
